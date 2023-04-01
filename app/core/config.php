4e04352c594520fdc117ed6c9034c5381299a40e
Huỳnh Nguyên Phúc <98326298+HuynhNguyenPhuc@users.noreply.github.com>
2023-04-01 10:46:00 +0700

    Add config.php again

---
+++ app/core/config.php
@@ -0,0 +1,33 @@
+<?php  
+
+if($_SERVER['SERVER_NAME'] == 'localhost')
+{
+	/** database config **/
+	define('DBNAME', 'my_db');
+	define('DBHOST', 'localhost');
+	define('DBUSER', 'root');
+	define('DBPORT', '3307');
+	define('DBPASS', '');
+	define('DBDRIVER', '');
+	
+	define('ROOT', 'http://localhost/WebAssignment/public');
+
+}else
+{
+	/** database config **/
+	define('DBNAME', 'my_db');
+	define('DBHOST', 'localhost');
+	define('DBUSER', 'root');
+	define('DBPASS', '');
+	define('DBPORT', '3307');
+	define('DBDRIVER', '');
+
+	define('ROOT', 'https://www.yourwebsite.com');
+
+}
+
+define('APP_NAME', "My Webiste");
+define('APP_DESC', "Best website on the planet");
+
+/** true means show errors **/
+define('DEBUG', true);
