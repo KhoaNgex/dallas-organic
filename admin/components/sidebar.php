c25b885dbade6f36925501895e86e220e768c424
Congtran1452002 <79902179+Congtran1452002@users.noreply.github.com>
2023-04-24 12:39:50 +0700

    add admin

---
+++ admin/components/sidebar.php
@@ -0,0 +1,58 @@
+<div class="sidebar">
+    <ul>
+        <li>
+            <a href="dashboard.php">
+                <i class="icon fa fa-th-large"></i>
+                <p>Dashboard</p>
+            </a>
+        </li>
+        <li>
+            <a href="product.php">
+                <i class="icon fa fa-shopping-bag"></i>
+                <p>Sản phẩm</p>
+            </a>
+        </li>
+        <li>
+            <a href="order.php">
+                <i class="icon fa fa-shopping-cart"></i>
+                <p>Phân loại sản phẩm</p>
+            </a>
+        </li>
+        <li>
+            <a href="order.php">
+                <i class="icon fa fa-receipt"></i>
+                <p>Đơn hàng</p>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="icon fa fa-coins"></i>
+                <p>Doanh thu</p>
+            </a>
+        </li>
+        <li>
+            <a href="account.php">
+                <i class="icon fa fa-users"></i>
+                <p>Tài khoản</p>
+            </a>
+        </li>
+        <!-- <li>
+            <a href="#">
+                <i class="icon fa fa-user-cog"></i>
+                <p>Khách hàng</p>
+            </a>
+        </li> -->
+        <li>
+            <a href="#">
+                <i class="icon fa fa-chart-line"></i>
+                <p>Báo cáo</p>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="icon fa fa-globe"></i>
+                <p>Website</p>
+            </a>
+        </li>
+    </ul>
+</div>
